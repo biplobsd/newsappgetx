d182c611e092e1df8d87d64b716b17aa59219f7a
Biplob Sutradhar <biplobsd11@gmail.com>
2022-05-28 19:09:37 +0600

    newsResponse model

---
+++ lib/core/model/news_response_model.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:newsappgetx/core/model/article.dart';
+
+part 'news_response_model.g.dart';
+
+@JsonSerializable()
+class NewsResponse {
+  @JsonKey(name: 'status')
+  late String status;
+
+  @JsonKey(name: 'totalResults')
+  late String totalResults;
+
+  @JsonKey(name: 'articles')
+  late List<Article> articles;
+  NewsResponse({
+    required this.status,
+    required this.totalResults,
+    required this.articles,
+  });
+
+  /// Connect the generated [_$PersonFromJson] function to the `fromJson`
+  /// factory.
+  factory NewsResponse.fromJson(Map<String, dynamic> json) =>
+      _$NewsResponseFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$NewsResponseToJson(this);
+}
