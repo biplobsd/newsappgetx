0f4c1e1b632c73533628a8523961077195892e76
Biplob Sutradhar <biplobsd11@gmail.com>
2022-05-28 18:41:53 +0600

    news repository

---
+++ lib/core/repository/news_repo_impl.dart
@@ -0,0 +1,15 @@
+import 'package:newsappgetx/core/repository/news_repository.dart';
+
+class newsRepoImpl implements NewsRepo {
+  @override
+  Future getNewsHeadline() {
+    // TODO: implement getNewsHeadline
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getSearchNews(String query) {
+    // TODO: implement getSearchNews
+    throw UnimplementedError();
+  }
+}
