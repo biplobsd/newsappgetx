bd8e1985c23ac3f659abaf5a16f2833a879282c4
Biplob Sutradhar <biplobsd11@gmail.com>
2022-05-29 07:57:07 +0600

    httpService to get

--- lib/core/repository/news_repo_impl.dart
+++ lib/core/repository/news_repo_impl.dart
@@ -1,6 +1,16 @@
+import 'package:get/get.dart';
 import 'package:newsappgetx/core/repository/news_repository.dart';
+import 'package:newsappgetx/service/http_services.dart';
+import 'package:newsappgetx/service/http_services_impl.dart';
 
 class newsRepoImpl implements NewsRepo {
+  late HttpService _httpService;
+
+  newsRepoImpl() {
+    _httpService = Get.put(HttpServiceImpl());
+    _httpService.init();
+  }
+
   @override
   Future getNewsHeadline() {
     // TODO: implement getNewsHeadline
