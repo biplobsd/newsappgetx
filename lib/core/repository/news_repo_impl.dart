02eb1f1ce282507867c98b31bfb31ecc12b34bb8
Biplob Sutradhar <biplobsd11@gmail.com>
2022-05-29 08:03:48 +0600

    repository impl

--- lib/core/repository/news_repo_impl.dart
+++ lib/core/repository/news_repo_impl.dart
@@ -1,25 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:newsappgetx/core/model/news_response_model.dart';
 import 'package:newsappgetx/core/repository/news_repository.dart';
 import 'package:newsappgetx/service/http_services.dart';
 import 'package:newsappgetx/service/http_services_impl.dart';
 
-class newsRepoImpl implements NewsRepo {
+class NewsRepoImpl implements NewsRepo {
   late HttpService _httpService;
 
-  newsRepoImpl() {
+  NewsRepoImpl() {
     _httpService = Get.put(HttpServiceImpl());
     _httpService.init();
   }
 
   @override
-  Future getNewsHeadline() {
-    // TODO: implement getNewsHeadline
-    throw UnimplementedError();
+  Future getNewsHeadline() async {
+    try {
+      final response =
+          await _httpService.getRequest('/v2/top-headlines?country=us');
+      final parseResponse = NewsResponse.fromJson(response.data);
+      return parseResponse.articles;
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
   }
 
   @override
-  Future getSearchNews(String query) {
-    // TODO: implement getSearchNews
-    throw UnimplementedError();
+  Future getSearchNews(String query) async {
+    try {
+      final response = await _httpService.getRequest('/v2/everything?q=$query');
+      final parseResponse = NewsResponse.fromJson(response.data);
+      return parseResponse.articles;
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
   }
 }
